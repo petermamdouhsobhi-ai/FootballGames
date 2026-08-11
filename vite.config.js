bc6463348ad1b4035968d2963de8c0e2e8c0c036
petermamdouhsobhi-ai <petermamdouhsobhi@gmail.com>
2026-08-11 14:55:27 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// على Vercel الموقع بيتنشر على الدومين نفسه، فـ base بيفضل "/"
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-});
